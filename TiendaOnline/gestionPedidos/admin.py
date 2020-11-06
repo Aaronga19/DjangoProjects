b54207cd233df47e926a8229f5312a7b645c7f2b
Aarón Juárez <66045880+Aaronga19@users.noreply.github.com>
2020-11-05 19:42:46 -0600

    Se ha completado la parte del ADMIN con Django

--- TiendaOnline/gestionPedidos/admin.py
+++ TiendaOnline/gestionPedidos/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from gestionPedidos.models import *
 
 # Register your models here.
+
+class ClientesAdmin(admin.ModelAdmin):
+    list_display=("nombre", "dirección", "telefono")
+    search_fields= ("nombre","telefono") 
+
+
+class ArticulosAdmin(admin.ModelAdmin):
+    list_display=("nombre","seccion","precio")
+    search_fields= ("nombre","seccion") 
+    list_filter= ("seccion",)
+
+class FechaAdmin(admin.ModelAdmin):
+    list_display=("numero", "fecha", "entregado")
+    list_filter=("fecha",)
+    date_hierarchy="fecha"
+
+
+admin.site.register(Clientes, ClientesAdmin)
+admin.site.register(Articulos, ArticulosAdmin)
+admin.site.register(Pedidos, FechaAdmin)
+
--- TiendaOnline/gestionPedidos/models.py
+++ TiendaOnline/gestionPedidos/models.py
@@ -4,14 +4,21 @@ from django.db import models
 
 class Clientes(models.Model):
     nombre = models.CharField(max_length=30)
-    dirección = models.CharField(max_length=50)
-    email = models.EmailField(max_length=25)
+    #apellidos = models.CharField(max_length=30)
+    dirección = models.CharField(max_length=50,verbose_name="Su dirección")
+    email = models.EmailField(max_length=25,blank=True,null=True)
     telefono = models.CharField(max_length=10)
+
+    def __str__(self):
+        return f"{self.nombre}"
 
 class Articulos(models.Model):
     nombre = models.CharField(max_length=30)
     seccion = models.CharField(max_length=20)
     precio = models.IntegerField()
+
+    def __str__(self):
+        return f'El nombre es --{self.nombre}-- la sección es --{self.seccion}-- y el precio es --{self.precio}--\n'
 
 class Pedidos(models.Model):
     numero = models.IntegerField()
